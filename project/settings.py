391b09db7a4346ef37656a513526be9ddbb45270
Bencodes1 <256.ben@gmail.com>
2021-03-31 17:45:52 -0400

    changed one word

--- project/settings.py
+++ project/settings.py
@@ -38,7 +38,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'api',
-    'restframework',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
